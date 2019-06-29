f489799e3884aa79c74fe37346c502e6a8f845a8
Icaro Oliveira <icarooliv2@gmail.com>
2019-06-29 19:47:52 -0300

    :art: (structure) fix modules controllers exports

--- src/modules/group/group.module.ts
+++ src/modules/group/group.module.ts
@@ -6,10 +6,12 @@ import { UserModule } from '../user/user.module';
 import { TopicModule } from '../topic/topic.module';
 import { PassportModule } from '@nestjs/passport';
 import { HabitModule } from '../habit/habit.module';
+import { GroupController } from './group.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Group]), UserModule, TopicModule, HabitModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   providers: [GroupService],
+  controllers: [GroupController],
   exports: [GroupService],
 })
 export class GroupModule {}
